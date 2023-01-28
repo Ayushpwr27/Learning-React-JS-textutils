48cb39b540da70659dd88daa9d86c4ba7f3a7705
Ayush Pawar <ayushpawar27@gmail.com>
2023-01-29 00:12:44 +0530

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/Components/About.js 	modified:   src/Components/Navbar.js 	modified:   src/Components/TextForms.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-import './App.css';
-import Navbar from './Components/Navbar';
-import TextForms from './Components/TextForms';
+import "./App.css";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+// import TextForms from './Components/TextForms';
 function App() {
   return (
-    
     <div className="App">
-
-      <Navbar title = "textutils" aboutText="About text utils"/>    
+      <Navbar title="textutils" aboutText="About text utils" />
       <div className="container">
-        <TextForms heading="Enter Text area to analyze"/> 
-      </div> 
-
-  </div>
-
+        {/* <TextForms heading="Enter Text area to analyze"/>  */}
+        <About />
+      </div>
+    </div>
   );
 }
 
--- src/Components/TextForms.js
+++ src/Components/TextForms.js
@@ -13,28 +13,72 @@ export default function TextForms(props) {
         // console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
-}
+    }
+    const ClearText =() =>{
+        // console.log("Uppercase was clicked" + text);
+        setText('')
+    }
     const handleOnChange =(event) =>{
         // console.log("On change");
         setText(event.target.value);
+    }
+
+    function wordCount(){
+        let words;
+        if (text === '') {
+            words = 0;
+        }
+        else{
+            words = text.split(" ").length;
+        }
+        return words
+    }
+    function sentenceCount(){
+        let words;
+        if (text === '') {
+            words = 0;
+        }
+        else if(text.split(".").length > 1){
+            words = text.split(".").length - 1;
+        }
+        else{
+            words = text.split(".").length;
+        } 
+        return words
+    }
+    function readingTime(){
+        let readTime;
+        if (text === '') {
+            readTime = 0;
+        }
+        else{
+            readTime = 0.008 *text.split(" ").length;
+        }
+        return readTime
     }
     const [text, setText] = useState('');  
         
     return (
         <>
             <div className='container'>
-                <div className="mb-3">
+                <div className="mb-3 my-5">
                 <h1>{props.heading} </h1>
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="Mybox" rows="3"></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleUpClick}> Convert to Uppercase</button>
-                <button className="btn btn-primary mx-5" onClick={handleLoClick}> Convert to Lowercase</button>
+                <button className="btn btn-primary mx-3" onClick={handleLoClick}> Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={ClearText}> Clear text</button>
+
+                
+
 
             </div>
             <div className="container my-3">
                 <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words and {text.length} Characters</p>
-                <p>{0.008 *text.split(" ").length} Minutes read</p>
+                <p>{wordCount()} words and {text.length} Characters</p>
+                <p>{readingTime()} Minutes read</p>
+                <p>{sentenceCount()} Sentence</p>
+
                 <h2>Preview</h2>
                 <p>{text}</p>
             </div>
