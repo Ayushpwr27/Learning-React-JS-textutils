e9904af0aaad9b8ef47216d5ab9a5acccd1239d8
Ayush Pawar <ayushpawar27@gmail.com>
2023-01-31 09:55:34 +0530

    modified:   src/App.js 	modified:   src/Components/Navbar.js

--- src/App.js
+++ src/App.js
@@ -45,9 +45,9 @@ function App() {
       <Alert alert = {alert}/>
       <div className="container my-3">
       <Routes>
-        <Route path="/about" element= {<About />}>
+        <Route path="/about" exact element= {<About />}>
         </Route>
-        <Route path="/" element = {<TextForms showAlert={showAlert} heading="Enter Text area to analyze" mode={mode} />}>
+        <Route path="/" exact element = {<TextForms showAlert={showAlert} heading="Enter Text area to analyze" mode={mode} />}>
         </Route>
       </Routes>
       </div>
